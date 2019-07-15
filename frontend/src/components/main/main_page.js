169e32ceed277f0c31b95f517a27bd0c46bc5241
ianellison <ianeliison87@yahoo.com>
2019-07-15 10:48:53 -0700

    finish user auth, start frontend for splash

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -5,9 +5,9 @@ class MainPage extends React.Component {
   render() {
     return (
       <div>
-        <p>A Twitter Clone</p>
+        <p>Look fashionable you slob</p>
         <footer>
-          Copyright &copy; 2019 Chirper
+          Copyright &copy; 2019 Outfittr
         </footer>
       </div>
     );
