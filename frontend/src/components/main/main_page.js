3538fa5923f2657b78423249655c8b2bd738ea6e
Msheezi <marshall.randolph@gmail.com>
2019-07-15 12:23:31 -0700

    Add some styling to splash page

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,15 +1,39 @@
 import React from 'react';
+import './main.css'
 
 class MainPage extends React.Component {
 
   render() {
     return (
-      <div>
-        <p>Look fashionable you slob</p>
+     <div>
+
+        <div className="splash-ctr-txt">
+          <span>
+          <p>The Premiere Outfit</p>
+          <p>Generator Web App</p>
+          <p>That keeps You Looking Sharp</p>
+
+          </span>
+        </div>
+     <div className="splash-container">
+        <br/>
+        <div className="splash-left-box">
+          <h3> What is Outfittr?</h3>
+          <p>Outfittr is a wardrobe app that generates an outfit for you to wear each day.  
+            Outfits will be tailored from your clorthes in your wardrobe based on guidelines uyou give it, 
+            and other data like weather.
+          </p>
+          <h3> Try our Demo User Login and generate an outfit today!!!</h3>
+        </div>
+        <div className="splash-right-box">
+
+        </div>
+
+      </div>
         <footer>
           Copyright &copy; 2019 Outfittr
         </footer>
-      </div>
+     </div>
     );
   }
 }
