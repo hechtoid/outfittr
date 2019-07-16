14958235d4ff5303e3c7ac6ffaea0ceffe94da9a
Samuel Phineas Hecht <hechtsp@gmail.com>
2019-07-16 10:57:38 -0700

    fix name to handle

--- routes/api/users.js
+++ routes/api/users.js
@@ -75,7 +75,7 @@ router.post('/register', (req, res) => {
         bcrypt.compare(password, user.password)
         .then(isMatch => {
             if (isMatch) {
-            const payload = {id: user.id, name: user.name};
+            const payload = {id: user.id, handle: user.handle};
 
             jwt.sign(
                 payload,
