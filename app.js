28e0de23a5de77d1d8ce75e0a4561d15d3292cb3
Samuel Phineas Hecht <hechtsp@gmail.com>
2019-07-15 17:50:37 -0700

    app use

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const passport = require('passport');
 
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
+const pants = require("./routes/api/pants");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -23,6 +24,7 @@ app.use(bodyParser.json());
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
+app.use("/api/pants", pants);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
