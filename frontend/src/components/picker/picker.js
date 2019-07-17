5f73d6cb188b9abdfd0569121068ee712404bfc2
Samuel Phineas Hecht <hechtsp@gmail.com>
2019-07-16 23:15:28 -0700

    pick obj via idx

--- frontend/src/components/picker/picker.js
+++ frontend/src/components/picker/picker.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import GarmentBox from '../garments/garment_box';
+import ShowBox from './show_box';
 
 class AddTop extends React.Component {
   constructor(props) {
       super(props);
 
       this.state = {
-          name: "",
-          color: "white",
-          hot: true,
-          formal: false,
-          wet: false,
-          athleisure: false  
+          // name: "",
+          // color: "white",
+          // hot: true,
+          // formal: false,
+          // wet: false,
+          // athleisure: false  ,
+          top: {},
+          pant: {}
       }
       document.title = 'pickr @ outfittr'
       this.handleSubmit = this.handleSubmit.bind(this);
@@ -24,132 +26,62 @@ class AddTop extends React.Component {
   handleSubmit(e) {
     e.preventDefault();
     let fit = {
-      name: this.state.name,
-      color: this.state.color,
-      hot: this.state.hot,
-      formal: this.state.formal,
-      wet: this.state.wet,
-      athleisure: this.state.athleisure
+      // name: this.state.name,
+      // color: this.state.color,
+      // hot: this.state.hot,
+      // formal: this.state.formal,
+      // wet: this.state.wet,
+      // athleisure: this.state.athleisure,
+      top: {},
+      pant: {}
     };
 
     // this.props.composeFit(fit); 
     // this.setState({name: ''});
     // window.location.reload();
   }
-
-  updateName() {
+  updateTop() {
     return e => this.setState({
-      name: e.currentTarget.value
-    });
-  }
-  updateColor() {
-    return e => this.setState({
-      color: e.currentTarget.value
-    });
-  }
-  updateCold() {
-    return e => this.setState({
-      hot: !e.currentTarget.checked
-    });
-  }
-  updateFormal() {
-    return e => this.setState({
-      formal: e.currentTarget.checked
-    });
-  }
-  updateWet() {
-    return e => this.setState({
-      wet: e.currentTarget.checked
-    });
-  }
-  updateAthleisure() {
-    return e => this.setState({
-      athleisure: e.currentTarget.checked
+      top: this.props.tops[e.target.value]
     });
   }
 
   render() {
+    // const topChoices = {for (let index = 0; index < this.props.tops.length; index++) {
+    //   return(`<option value="${index}">${this.props.tops[index].name}</option>`)
+    // }}
+    // // {topChoices}
+    let tops=this.props.tops
     return (
       <div className="picker-page">
-        {/* <div className="create-form"> */}
-          {/* <form onSubmit={this.handleSubmit}>
-            <input type="text"
-              className="clothing-input"
-              value={this.state.name}
-              onChange={this.updateName()}
-              placeholder="Name your outfit"
-            />
-            <select className="garment-checkbox" className="color-btn" value={this.state.color} onChange={this.updateColor()}>
-              <option value="white">white</option>
-              <option value="red">red</option>
-              <option value="orange">orange</option>
-              <option value="yellow">yellow</option>
-              <option value="green">green</option>
-              <option value="blue">blue</option>
-              <option value="indigo">indigo</option>
-              <option value="violet">violet</option>
-              <option value="black">black</option>
-            </select>
-            <div className="radios">
-              <div className="form-left">
-                <label>
-                  Hot?
-                    <input type="checkbox"
-                    className="garment-checkbox"
-                    onChange={this.updateCold()}
-                    checked={!this.state.hot}
-                  />
-                </label>
-                <label>
-                  Formal?
-                  <input type="checkbox"
-                    className="garment-checkbox"
-                    onChange={this.updateFormal()}
-                    checked={this.state.formal}
-                  />
-                </label>
-              </div>
-              <div className="form-right">
-                <label>
-                  Athleisure?
-                  <input type="checkbox"
-                    className="garment-checkbox"
-                    onChange={this.updateAthleisure()}
-                    checked={this.state.athleisure}
-                  />
-                </label>
-                <label>
-                  Rain?
-                  <input type="checkbox"
-                    className="garment-checkbox"
-                    onChange={this.updateWet()}
-                    checked={this.state.wet}
-                  />
-                </label>
-              </div>
-            </div>
-            <input className="submit-btn" type="submit" value="Submit" />
-          </form> */}
-        {/* </div> */}
-        <br />
-        <select className="garment-checkbox" className="color-btn" value={this.state.color} onChange={this.updateColor()}>
-          <option value="white">white</option>
-          <option value="red">red</option>
-          <option value="orange">orange</option>
-          <option value="yellow">yellow</option>
-          <option value="green">green</option>
-          <option value="blue">blue</option>
-          <option value="indigo">indigo</option>
-          <option value="violet">violet</option>
-          <option value="black">black</option>
+        <select className="picker-menu" 
+        value={this.state.top}
+        onChange={this.updateTop()}
+        >
+          {tops.map((top,index) => (
+
+            // <div className="garment-item">
+            //   <GarmentBox
+            //     key={top._id}
+            //     name={top.name}
+            //     color={top.color}
+            //     type="top"
+            //   />
+            // </div>
+          <option value={index}>{top.name}</option>
+          ))}
         </select>
+
+        name={this.state.top.name}
+        color={this.state.top.color}
         <div className="garment-container">
           <div className="demo-item">
-            <GarmentBox
-              name={this.state.name}
-              color={this.state.color}
+            <ShowBox
+              name={this.state.top.name}
+              color={this.state.top.color}
               type="top"
             />
+            
           </div>
           <br/>
           {/* {this.props.tops.map(top => (
---
+++ frontend/src/components/picker/show_box.js
@@ -0,0 +1,159 @@
+import React from 'react';
+import '../garments/garment_box.css';
+import black_pant from '../garments/black_pant.png'
+import blue_pant from '../garments/blue_pant.png'
+import green_pant from '../garments/green_pant.png'
+import orange_pant from '../garments/orange_pant.png'
+import indigo_pant from '../garments/indigo_pant.png'
+import red_pant from '../garments/red_pant.png'
+import violet_pant from '../garments/violet_pant.png'
+import white_pant from '../garments/white_pant.png'
+import yellow_pant from '../garments/yellow_pant.png'
+import black_top from '../garments/black_top.png'
+import blue_top from '../garments/blue_top.png'
+import green_top from '../garments/green_top.png'
+import orange_top from '../garments/orange_top.png'
+import indigo_top from '../garments/indigo_top.png'
+import red_top from '../garments/red_top.png'
+import violet_top from '../garments/violet_top.png'
+import white_top from '../garments/white_top.png'
+import yellow_top from '../garments/yellow_top.png'
+
+class ShowBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cssClass: '',
+      image: '',
+      color: this.props.color,
+      imageName: `${this.props.color}_${this.props.type}`
+    }
+  }
+
+switcher(){
+  switch (this.state.imageName) {
+      case 'white_top':
+        this.setState({
+          image: `${white_top}`
+        })
+        break;
+      case 'red_top':
+        this.setState({
+          image: `${red_top}`
+        })
+        break;
+      case 'orange_top':
+        this.setState({
+          image: `${orange_top}`
+        })
+        break;
+      case 'yellow_top':
+        this.setState({
+          image: `${yellow_top}`
+        })
+        break;
+      case 'green_top':
+        this.setState({
+          image: `${green_top}`
+        })
+        break;
+      case 'blue_top':
+        this.setState({
+          image: `${blue_top}`
+        })
+        break;
+      case 'indigo_top':
+        this.setState({
+          image: `${indigo_top}`
+        })
+        break;
+      case 'violet_top':
+        this.setState({
+          image: `${violet_top}`
+        })
+        break;
+      case 'black_top':
+        this.setState({
+          image: `${black_top}`
+        })
+        break;
+      case 'white_pant':
+        this.setState({
+          image: `${white_pant}`
+        })
+        break;
+      case 'red_pant':
+        this.setState({
+          image: `${red_pant}`
+        })
+        break;
+      case 'orange_pant':
+        this.setState({
+          image: `${orange_pant}`
+        })
+        break;
+      case 'yellow_pant':
+        this.setState({
+          image: `${yellow_pant}`
+        })
+        break;
+      case 'green_pant':
+        this.setState({
+          image: `${green_pant}`
+        })
+        break;
+      case 'blue_pant':
+        this.setState({
+          image: `${blue_pant}`
+        })
+        break;
+      case 'indigo_pant':
+        this.setState({
+          image: `${indigo_pant}`
+        })
+        break;
+      case 'violet_pant':
+        this.setState({
+          image: `${violet_pant}`
+        })
+        break;
+      case 'black_pant':
+        this.setState({
+          image: `${black_pant}`
+        })
+        break;
+      default:
+        this.setState({
+          image: `${white_top}`
+        })
+    }
+}
+componentWillReceiveProps(nextProps) {
+    this.setState({
+      imageName: `${nextProps.color}_${nextProps.type}`,
+      color: `${this.props.color}`})
+    this.switcher()
+  }
+
+  // componentDidUpdate(){
+  //   if (this.props.color !== this.state.color){
+  //     this.setState({ imageName: `${this.props.color}_${this.props.type}`, color: `${this.props.color}` })
+  //     this.switcher()
+  //   }
+  // }
+
+componentDidMount(){
+  this.switcher()
+  }
+
+  render() {
+    return (
+      <div className={this.state.cssClass}>
+        <div > {this.props.color} {this.props.type} called {this.props.name}.
+        </div>
+        <img className="garment-img" src={this.state.image} alt={this.props.color}></img>
+        </div>
+    );
+  }
+}
+export default ShowBox;
