3be1f278fed9b8d428fd2abafd624b93e3fad1f1
Samuel Phineas Hecht <hechtsp@gmail.com>
2020-03-13 12:57:12 -0700

    swich hash router to real router

--- frontend/src/components/root.js
+++ frontend/src/components/root.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import App from './app';
 
 const Root = ({ store }) => (
   <Provider store={ store }>
-    <HashRouter>
+    <Router>
       <App />
-    </HashRouter>
+    </Router>
   </Provider>
 );
 
